8b4a7069e16a98ebc94307cb7363e3661b62e49a
arestus <arestus@gmail.com>
2021-04-23 22:06:23 +0300

    add loader and table-info

---
+++ src/components/CopyToClipBoardText/index.js
@@ -0,0 +1,64 @@
+import { useState, useCallback } from "react";
+import propTypes from "prop-types";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import { useCopyToClipboard } from "react-use";
+import Button from "@material-ui/core/Button";
+import FileCopyOutlinedIcon from "@material-ui/icons/FileCopyOutlined";
+import Tooltip from "@material-ui/core/Tooltip";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      cursor: "pointer",
+    },
+    icon: {
+      marginRight: theme.spacing(4),
+    },
+  })
+);
+
+const STATUS_COPY = {
+  COPY: "copy",
+  COPIED: "copied",
+};
+
+const TITLE_BY_STATUS = {
+  [STATUS_COPY.COPY]: "Copy",
+  [STATUS_COPY.COPIED]: "Copied",
+};
+
+export const CopyToClipboardText = ({ text }) => {
+  const classes = useStyles();
+  const [, copyToClipboard] = useCopyToClipboard();
+  const [statusCopy, setStatusCopy] = useState("copy");
+
+  const onClickCopy = useCallback(() => {
+    copyToClipboard(text);
+    setStatusCopy(STATUS_COPY.COPIED);
+  }, [copyToClipboard, text]);
+
+  const onClickAway = useCallback(() => {
+    setStatusCopy(STATUS_COPY.COPY);
+  }, [setStatusCopy]);
+
+  return (
+    <ClickAwayListener onClickAway={onClickAway}>
+      <Tooltip title={TITLE_BY_STATUS[statusCopy]} placement="top" arrow>
+        <Button
+          display="flex"
+          alignItems="center"
+          className={classes.root}
+          onClick={onClickCopy}
+        >
+          <FileCopyOutlinedIcon fontSize="small" className={classes.icon} />
+          {text}
+        </Button>
+      </Tooltip>
+    </ClickAwayListener>
+  );
+};
+
+CopyToClipboardText.propTypes = {
+  text: propTypes.string.isRequired,
+};
--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,3 +1,46 @@
+import { useContacts } from "./useContacts";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import { ContactsTable } from "./ContactsTable";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      marginTop: theme.spacing(4),
+    },
+    headContainer: {
+      marginBottom: theme.spacing(3),
+    },
+  })
+);
+
 export const Contacts = () => {
-  return <div>Contacts</div>;
+  const classes = useStyles();
+  const contacts = useContacts();
+
+  return (
+    <Container className={classes.root}>
+      <Grid container>
+        <Grid item xs={12} className={classes.headContainer}>
+          <Typography variant="h3" component="h1">
+            Contacts
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          {(() => {
+            if (contacts.isLoading) {
+              return <CircularProgress />;
+            }
+            if (contacts.isError) {
+              return <div>...error</div>;
+            }
+            return <ContactsTable data={contacts.data} />;
+          })()}
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
